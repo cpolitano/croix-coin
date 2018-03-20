252f6cd43b13da4f0dff436dbc5a7de8987b8398
Clare Politano Hutchings <cpolitano8@gmail.com>
2018-03-20 16:57:31 -0700

    init and transfer

--- croixContract.sol
+++ croixContract.sol
@@ -1,6 +1,15 @@
 pragma solidity ^0.4.19;
 
-contract croixCoin {
+contract CroixCoin {
   /* mapping all the accounts and balances of croixCoin */
   mapping (address => uint256) public balanceOf;
+
+  function CroixCoin(uint initialSupply) public {
+    balanceOf[msg.sender] = initialSupply;
+  }
+
+  function transfer(address _to, uint256 _value) {
+    balanceOf[msg.sender] -= _value;
+    balanceOf[_to] += _value;
+  }
 }
